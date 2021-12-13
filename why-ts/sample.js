46dc1bdbc64914f4e27696128f0427e63315b11f
DevFrog92 <yongsu1481@gmail.com>
2021-12-13 22:25:00 +0900

    add: why ts?

---
+++ why-ts/sample.js
@@ -0,0 +1,20 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const result = add()
+// result.toLocaleString()
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+
+function sum(a, b) {
+  return a + b;
+}
+
+sum(10, 20);
