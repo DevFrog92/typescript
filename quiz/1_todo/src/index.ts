1fc7f6c3b2c5644894dc635bfaf16466eb1c81df
DevFrog92 <yongsu1481@gmail.com>
2021-12-15 21:05:57 +0900

    add: interface and interface extends

---
+++ quiz/1_todo/src/index.ts
@@ -0,0 +1,82 @@
+// // type을 지정하는 방법
+// type Todo = {
+//   id: number;
+//   titile: string;
+//   done: boolean;
+// };
+
+// type이 아닌 interface로 지정하는 방법
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
+
+// api
+function fetchTodoItems(): Todo[] {
+  const todos = [
+    // shape { id: number, title: string, done: boolean }
+    { id: 1, title: '안녕', done: false },
+    { id: 2, title: '타입', done: false },
+    { id: 3, title: '스크립트', done: false },
+  ];
+  return todos;
+}
+
+// crud methods
+function fetchTodos(): object[] {
+  const todos = fetchTodoItems();
+  return todos;
+}
+// void 는 반환값이 없다.
+function addTodo(todo: Todo): void {
+  todoItems.push(todo);
+}
+
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1);
+}
+
+function completeTodo(index: number, todo: Todo): void {
+  todo.done = true;
+  todoItems.splice(index, 1, todo);
+}
+
+// business logic
+function logFirstTodo(): object {
+  return todoItems[0];
+}
+
+function showCompleted(): Array<object> {
+  return todoItems.filter((item: Todo) => item.done);
+}
+
+// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
+function addTwoTodoItems(): void {
+  const task01 = {
+    id: 4,
+    title: 'javascript',
+    done: false,
+  };
+
+  const task02 = {
+    id: 5,
+    title: 'typescript',
+    done: false,
+  };
+
+  addTodo(task01);
+  addTodo(task02);
+  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+}
+
+// NOTE: 유틸 함수
+function log(): void {
+  console.log(todoItems);
+}
+
+todoItems = fetchTodoItems();
+addTwoTodoItems();
+log();
