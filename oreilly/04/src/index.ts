a944b2507206fd0ce60f56fc211588292d1923fe
DevFrog92 <yongsu1481@gmail.com>
2022-04-21 19:22:37 +0900

    🌱 chapter04 boilerplate 🌱 function and generator example

---
+++ oreilly/04/src/index.ts
@@ -0,0 +1,73 @@
+// 반환 타입은 자동으로 추론 되지만, 원하면 명시할 수 있다.
+
+function add(a: number, b:number):number {
+  return a + b
+}
+
+function greet(name: string) {
+  return `hello ${name}`
+}
+
+let greet2 = function (name: string) {
+  return `hello ${name}`
+}
+
+let greet3 = (name: string) => {
+  return `hello ${name}`
+}
+
+let greet4 = (name: string) => `hello ${name}`
+
+let greet5 = new Function('name', 'return "hello" + name')
+
+
+type Context = {
+  appId?: string
+  userId?: string
+}
+
+function log(message: string, context:Context = {}) {
+  let time = new Date().toISOString()
+  console.log(time, message, context.userId)
+}
+
+log('hello', {appId: "ddfe", userId: "rew"})
+
+function sum(...numbers: number[]) {
+  return Array.from(arguments)
+}
+
+console.log(sum(1,23,4))
+
+let this_func = {
+  a() {
+    return this
+  }
+}
+
+console.log(this_func.a())
+let a_func = this_func.a
+console.log(a_func())
+
+function fancyDate(this: Date) {
+  return `${this.getDate()}:${this.getMonth()}:${this.getFullYear}`
+}
+
+fancyDate.call(new Date)
+
+function* createFibonacciGenerator() {
+  let a = 0
+  let b = 1
+  while(true) {
+    yield a;
+    [a, b] = [b, a+b]
+  }
+}
+
+let fibonacciGenerator = createFibonacciGenerator()
+console.log(fibonacciGenerator.next())
+console.log(fibonacciGenerator.next())
+console.log(fibonacciGenerator.next())
+console.log(fibonacciGenerator.next())
+console.log(fibonacciGenerator.next())
+console.log(fibonacciGenerator.next())
