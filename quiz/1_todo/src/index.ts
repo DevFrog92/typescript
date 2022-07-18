61c0ee6d365b2e3a9126040a5ccc77faa0364523
DevFrog92 <yongsu1481@gmail.com>
2022-07-18 23:32:32 +0900

    🩹 Solve quiz #1

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,21 +1,19 @@
-// // type을 지정하는 방법
-// type Todo = {
+// type TODO = {
 //   id: number;
-//   titile: string;
+//   title: string;
 //   done: boolean;
 // };
 
-// type이 아닌 interface로 지정하는 방법
-interface Todo {
+interface TODO {
   id: number;
   title: string;
   done: boolean;
 }
 
-let todoItems: Todo[];
+let todoItems: TODO[];
 
 // api
-function fetchTodoItems(): Todo[] {
+function fetchTodoItems(): TODO[] {
   const todos = [
     // shape { id: number, title: string, done: boolean }
     { id: 1, title: '안녕', done: false },
@@ -26,12 +24,12 @@ function fetchTodoItems(): Todo[] {
 }
 
 // crud methods
-function fetchTodos(): object[] {
+function fetchTodos(): TODO[] {
   const todos = fetchTodoItems();
   return todos;
 }
 // void 는 반환값이 없다.
-function addTodo(todo: Todo): void {
+function addTodo(todo: TODO): void {
   todoItems.push(todo);
 }
 
@@ -39,34 +37,32 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: Todo): void {
+function completeTodo(index: number, todo: TODO): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo(): object {
+function logFirstTodo(): TODO {
   return todoItems[0];
 }
 
-function showCompleted(): Array<object> {
-  return todoItems.filter((item: Todo) => item.done);
+function showCompleted(): TODO[] {
+  return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(): void {
+function addTwoTodoItems() {
   const task01 = {
     id: 4,
     title: 'javascript',
     done: false,
   };
-
   const task02 = {
     id: 5,
     title: 'typescript',
     done: false,
   };
-
   addTodo(task01);
   addTodo(task02);
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
